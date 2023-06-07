a41c4de1ff23f31131560fdd105cc3e78de7b8f1
Niico <nicolas.gonz.mart@gmail.com>
2023-06-06 20:32:31 -0500

    añadiendo uuid, cambios controlles uuid

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,32 +1,44 @@
-import { Controller, Get, Param, Body, Post, ParseIntPipe, Delete, Patch} from '@nestjs/common';
-import { Car } from 'src/interfaces/car.interface';
+import {
+  Controller,
+  Get,
+  Param,
+  Body,
+  Post,
+  Delete,
+  Patch,
+  HttpException,
+  HttpStatus,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { Car } from 'src/cars/interfaces/car.interface';
 import { CarsService } from './cars.service';
 @Controller('cars')
 export class CarsController {
-    constructor(private readonly carsService: CarsService) {}
-    @Get()
-    async getCars(): Promise<Car[]> {
-        return this.carsService.getCars();
-    }
-    @Get(':id')
-    getCarById(@Param('id',ParseIntPipe) id:number) {
-        return this.carsService.getCarById(id);
-    }
-    
-    @Post()
-    async createCar(@Body() car: Car): Promise<any> {
-        return this.carsService.createCar(car);
-    }
+  constructor(private readonly carsService: CarsService) {}
+  @Get()
+  async getCars(): Promise<Car[]> {
+    return this.carsService.getCars();
+  }
+  @Get(':id')
+  getCarById(@Param('id', ParseUUIDPipe) id: string) {
+    return this.carsService.getCarById(id);
+  }
 
-    @Delete(':id')
-    async deleteCar(@Param('id',ParseIntPipe) id:number): Promise<any> {
-        return this.carsService.deleteCar(id);
-    }
+  @Post()
+  async createCar(@Body() car: Car): Promise<any> {
+    return this.carsService.createCar(car);
+  }
 
-    @Patch(':id')
-    async updateCar(@Param('id',ParseIntPipe) id:number, @Body() car: Car): Promise<any> {
-        return this.carsService.updateCar(id, car);
-    }
+  @Delete(':id')
+  async deleteCar(@Param('id', ParseUUIDPipe) id: string): Promise<any> {
+    return this.carsService.deleteCar(id);
+  }
 
-
+  @Patch(':id')
+  async updateCar(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() car: Car,
+  ): Promise<any> {
+    return this.carsService.updateCar(id, car);
+  }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,25 +1,25 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Car } from 'src/interfaces/car.interface';
-
+import { Car } from 'src/cars/interfaces/car.interface';
+import {v4 as uuid } from 'uuid';
 @Injectable()
 export class CarsService {
     private cars: Car[] = [
         {
-            id: 1,
+            id:uuid(),
             brand: 'Audi',
             color: 'red',
             model: 'A3',
             price: 20000
         },
         {
-            id: 2,
+            id:uuid(),
             brand: 'BMW',
             color: 'white',
             model: 'X5',
             price: 25000
         },
         {
-            id: 3,
+            id:uuid(),
             brand: 'Mercedes',
             color: 'black',
             model: 'CLA',
@@ -29,7 +29,7 @@ export class CarsService {
     getCars(): Car[] {
         return this.cars;
     }
-    getCarById(id: number): Car {
+    getCarById(id:string): Car {
         const car = this.cars.find(car => car.id === id);
         if(!car) throw  new NotFoundException(`Car whit id '${id}' not found`);
         return car
@@ -38,12 +38,12 @@ export class CarsService {
         this.cars.push(car);
     }
     
-    deleteCar(id: number) {
+    deleteCar(id:string) {
         const index = this.cars.findIndex(car => car.id === id);
         if(index === -1) throw new NotFoundException(`Car whit id '${id}' not found`);
         this.cars.splice(index, 1);
     }
-    updateCar(id: number, car: Car) {
+    updateCar(id:string, car: Car) {
         const index = this.cars.findIndex(car => car.id === id);
         if(index === -1) throw new NotFoundException(`Car whit id '${id}' not found`);
         this.cars[index] = car;
